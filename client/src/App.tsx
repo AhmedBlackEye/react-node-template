b1ed3c0652a565abd15a9577ac42cb6db821f1cc
ahmeding.hosam <ahmeding.hosam@gmail.com>
2024-08-12 12:17:45 +0000

    Improved template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [message, setMessage] = useState("...Loading");
   const updateMsg = async () => {
-    const res = await fetch("/api/");
+    const res = await fetch("/api/") ;
     const { message: msg } = await res.json();
     setMessage(msg);
   };
